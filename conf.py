daa3cdcfac0c37747615733bb76aec41d3f613d7
AMMOnium <ammonium@mail.ru>
2014-05-25 23:19:36 +0400

    config stub: demuxer option for mplayer to parse ID3v2 tags

--- conf.py
+++ conf.py
@@ -4,5 +4,5 @@ root = ["/path/to/media/files"]
 ## HTTP listener port
 port = 18080
 
-## use libavcodec to parse ID3 V2 tags
+## use libavcodec to parse ID3 V2 tags (for possible values see "mplayer -audio-demuxer help")
 demuxer = "lavf"
